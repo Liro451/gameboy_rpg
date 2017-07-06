19275736c529bfb8599573fc660adaca7c087e23
Liro451 <siro4510@gmail.com>
2017-07-06 16:58:44 +0800

    Implemented rudimentary walking animation

--- game.c
+++ game.c
@@ -6,6 +6,7 @@
 void start_game();
 void update_player();
 void move_player_sprite(int direction, int reversed);
+void walk(int direction);
 
 UINT8 player_x, player_y;
 UINT8 last_keys;
@@ -60,6 +61,22 @@ void move_player_sprite(int direction, int reversed)
 			move_sprite(3,player_x,player_y+8);
 			}
 	}
+
+}
+
+
+void walk(int direction) {
+	// Slow because of delays, need to find a way around this
+	
+	int tile;
+	int time = 100;
+	delay(time);
+	for(tile=0;tile<4;tile++)
+		{
+			set_sprite_tile(tile,tile + direction + 12);
+		}
+	delay(time);
+
 }
 
 void start_game()
@@ -117,6 +134,7 @@ void update_player()
 	if (joypad() & J_UP) {
 	 		player_y--;
 	 		move_player_sprite(4,0);
+	 		walk(4);
 			if (player_y == 15) {
 				player_y = 16;
 			}
@@ -126,6 +144,7 @@ void update_player()
 	if (joypad() & J_DOWN) {
 		player_y++;
 		move_player_sprite(0,0);
+		walk(0);
 			if (player_y == 153) {
 				player_y = 152;
 			}
@@ -135,6 +154,7 @@ void update_player()
 	if (joypad() & J_LEFT) {
 		player_x--;
 		move_player_sprite(8,0);
+		walk(8);
 			if (player_x == 7) {
 				player_x = 8;
 			}
@@ -144,6 +164,7 @@ void update_player()
 	if (joypad() & J_RIGHT) {
 		player_x++;
 		move_player_sprite(8,1);
+		walk(8);
 			if (player_x == 161) {
 				player_x = 160;
 			}
